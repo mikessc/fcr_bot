45fea0e4f3277d5bb8d4a3d0aa059f6dabb42a4a
DIR\m.salas.cespedes <m.salas.cespedes@accenture.com>
2018-01-31 10:27:32 -0600

    testing

--- fcr.js
+++ fcr.js
@@ -1,14 +1,32 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const fcr = new Discord.Client();
 
-client.on("ready", () => {
+fcr.on("ready", () => {
   console.log("I am ready!");
 });
 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+fcr.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.content.startsWith(".fcr")) {
+        message.channel.send("bot command!");
+    }
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
 });
 
-client.login("NDA4MjkwNDM3NzUzOTk1Mjc1.DVN8uQ.-ZimvU5rDnuVewf3RsOV149VZM8");
+fcr.login("NDA4MjkwNDM3NzUzOTk1Mjc1.DVN8uQ.-ZimvU5rDnuVewf3RsOV149VZM8");
