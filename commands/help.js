70f21abcbee5b505a46e03584bad9cf85b6cd278
DIR\m.salas.cespedes <m.salas.cespedes@accenture.com>
2018-02-26 14:07:34 -0600

    connect to external mongodb

--- commands/help.js
+++ commands/help.js
@@ -1,35 +1,37 @@
 exports.run = (params) => {
   var commands = {}; // initiate value for the command list of client
   
-  
-
   params.message.author.send({
     embed: {
       color: params.config.colors.blue,
       title: "AYUDA",
-      description: "Esta es la guía de comandos que puedes utilizar con el bot de Fortnite CR.",
+      description: "Esta es la guía de comandos que puedes utilizar con el bot de Fortnite CR.\nPara información más detallada visita este [enlace](https://github.com/mikessc/fcr_bot/blob/master/README.md)",
       fields: [{
-          name: ".fcr register FORTNITE_ID",
+          name: "+register FORTNITE_ID",
           value: "• Liga tu usuario de Discord con tu ID de Fortnite. Remplazar 'FORTNITE_ID' por tu usuario de Fortnite, no olvides asegurarte de que este bien escrito para que los demas jugadores puedan agregarte sin problemas"
         },
         {
-          name: ".fcr get @DISCORD_ID",
+          name: "+get @DISCORD_ID",
           value: "• Te muestra el ID de Fortnite de algun usuario del grupo. Remplazar 'DISCORD_ID' por algun ID de usuario de Discord."
         },
         {
-          name: "!squad",
+          name: "+reset FORTNITE_ID",
+          value: "• resetea tu ID de Fortnite. Remplazar 'FORTNITE_ID' por tu nuevo usuario de Fortnite."
+        },
+        {
+          name: "+squad",
           value: "• Mensaje general para pedir alguien en tu squad."
         },
         {
-          name: "!squad @DISCORD_ID",
+          name: "+squad @DISCORD_ID",
           value: "• Mensaje para pedir a alguien especifico en tu squad. Remplazar 'DISCORD_ID' por algun ID de usuario de Discord."
         },
         {
-          name: "!uber",
+          name: "+uber",
           value: "• Ofrecer carreo."
         },
         {
-          name: "!uber @DISCORD_ID",
+          name: "+uber @DISCORD_ID",
           value: "• Pedirle carreo a alguien. Remplazar 'DISCORD_ID' por algun ID de usuario de Discord."
         }
       ],
